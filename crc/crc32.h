1c46d122fe3e00d1526e3d4afd5b83c3345f39d8
cooldoger <cooldoger@gmail.com>
2012-11-25 10:35:17 -0800

    crc32 init

---
+++ crc/crc32.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+#include <sys/types.h>
+
+uint32_t crc32_raw(const void *buf, size_t size, uint32_t crc);
---
+++ crc/template.c
@@ -0,0 +1,29 @@
+/* 
+ * INSTRUCTIONS
+ *
+ * Refer to INSTRUCTIONS.txt for full instructions and examples.
+ */
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+
+#include <assert.h>
+#include <ctype.h>
+#include <err.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "crc32.h"
+
+int
+main(void)
+{
+	return (0);
+}
+

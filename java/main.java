72edd492558f255902217753b440d0bc6e39bf10
cooldoger <cooldoger@gmail.com>
2013-01-24 23:48:41 -0800

    Add java hello world

---
+++ java/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        Boolean i;
+        boolean j;
+        i = true;
+        j = false;
+        System.out.println("hello Jay");
+        System.out.println(j);
+    }
+}
